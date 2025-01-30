a842c05bbdeff23e210987ab2ca1700b1d4cf115
Erikson Gonçalves Sanchez <eriksongoncalves@yahoo.com.br>
2025-01-30 15:03:10 -0300

    feat: conta-estatisticas

--- src/app/conta/estatisticas/page.tsx
+++ src/app/conta/estatisticas/page.tsx
@@ -1,3 +1,28 @@
-export default function StatisticsPage() {
-  return <h1 className="title">Estatisticas</h1>
+import { Metadata } from "next"
+
+import statsGet from "@/actions/stats-get"
+
+import dynamic from "next/dynamic"
+
+const ContaEstatisticas = dynamic(
+  () => import("@/components/Conta/ContaEstatisticas/conta-estatisticas"),
+  {
+    loading: () => <p>Carregando...</p>,
+    ssr: false
+  }
+)
+
+export const metadata: Metadata = {
+  title: "Estatísticas | Minha Conta"
+}
+
+export default async function EstatisticasPage() {
+  const { data } = await statsGet()
+
+  if (!data) return null
+  return (
+    <section>
+      <ContaEstatisticas data={data} />
+    </section>
+  )
 }
